a56efdf384469f784d25571c4a33db9b8cf5a6db
Asif Mehmood <asif-mehmood@hotmail.com>
2023-03-30 17:14:52 +0500

    added joipassword validation

--- utils/userSchema.js
+++ utils/userSchema.js
@@ -1,4 +1,6 @@
 const Joi = require('joi');
+const { joiPasswordExtendCore } = require('joi-password');
+const joiPassword = Joi.extend(joiPasswordExtendCore);
 
 exports.signupSchema = Joi.object({
   first_name: Joi.string().alphanum().min(3).max(20).required(),
@@ -8,7 +10,16 @@ exports.signupSchema = Joi.object({
     .length(10)
     .pattern(/^[0-9]+$/)
     .required(),
-  password: Joi.string().min(10).required().label('password'),
+  // password: Joi.string().min(10).required().label('password'),
+  password: joiPassword
+    .string()
+    .minOfSpecialCharacters(1)
+    .minOfLowercase(1)
+    .minOfUppercase(1)
+    .minOfNumeric(5)
+    .noWhiteSpaces()
+    .required()
+    .label('password'),
   confirm_password: Joi.string()
     .valid(Joi.ref('password'))
     .required()
