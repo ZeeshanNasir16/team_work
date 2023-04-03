8f2dcfaf0aac531abc08826602738d57cbc1f357
Asif Mehmood <asif-mehmood@hotmail.com>
2023-04-03 15:03:54 +0500

    swagger documentation  optimized

--- controller/userController.js
+++ controller/userController.js
@@ -7,6 +7,53 @@ const {
 const logger = require('../utils/logger');
 const { SendMail } = require('../utils/SendMail');
 
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *        User:
+ *            type: object
+ *            properties:
+ *                first_name:
+ *                    type: string
+ *                    example: "asif"
+ *                last_name:
+ *                    type: string
+ *                    example: "mehmood"
+ *                email:
+ *                    type: string
+ *                    example: "asif-mehmood@hotmail.com"
+ *                mobile_no:
+ *                    type: string
+ *                    example: "1234567890"
+ *                password:
+ *                    type: string
+ *                    example: "asifM@12345"
+ *                confirm_password:
+ *                    type: string
+ *                    example: "asifM@12345"
+ *                user_type_id:
+ *                    type: integer
+ *                    example: "2"
+ */
+
+/**
+ * @swagger
+ * /users/register:
+ *  post:
+ *      summary: SignUp user
+ *      description: this api is used to add user data to database
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                   schema:
+ *                       $ref: '#components/schemas/User'
+ *      responses:
+ *          200:
+ *              description: user Signup successfully...
+ */
+
 exports.signUpUser = async (req, res) => {
   try {
     const registration_data = req.body;
@@ -26,6 +73,13 @@ exports.signUpUser = async (req, res) => {
     }
   } catch (error) {
     logger.error(error.message);
+    if (error.errno === -4039) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Email did not sent to the user due to connection error',
+        errorMessage: error.message,
+      });
+    }
     return res.status(404).json({
       status: 'fail',
       message:
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000/',
+        url: 'http://localhost:4000/',
       },
     ],
   },
