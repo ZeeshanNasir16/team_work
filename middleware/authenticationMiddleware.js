814c5ab082a542cfd05943bae0bc868740b26641
MominRiaz27 <Momin.Riaz27@gmail.com>
2023-04-04 19:55:42 +0500

    verify token updated

--- middleware/authenticationMiddleware.js
+++ middleware/authenticationMiddleware.js
@@ -1,10 +1,11 @@
 const jwt =  require('jsonwebtoken');
 const dotenv = require("dotenv");
 const HTTPCodes = require('../utils/responses');
+const logger = require('../utils/logger.js');
+
 dotenv.config();
 
 module.exports = function verifyToken(req,res,next) {
-    console.log("inside verify", req.headers)
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if (!token) {
@@ -12,16 +13,13 @@ module.exports = function verifyToken(req,res,next) {
         json({message: 'You are not authorized'});
     }
     jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
-        console.log("error: ", err)
-        console.log("decoded: ", decoded)
+        logger.info("error: ", err)
+        logger.info("decoded: ", decoded)
         if(!err){
-            console.log(decoded)
+            logger.info(decoded)
             if(decoded.id == 1){
-                // res.status(HTTPCodes.OK).
-                // json({message: 'You are authorized'});
-                console.log(" You're Authorized ")
+                logger.info(" You're Authorized ")
                 next()
-                
             }else{
                 res.status(HTTPCodes.NOT_AUTHORIZED).
                 json({message: 'You are Not authorized'});
